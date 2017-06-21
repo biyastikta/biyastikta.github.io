bc073a22c6a8f643292bf365b426ba4a3f4d9685
Cenk <cenk@hunili.com>
2017-06-21 10:20:48 +0300

    v1.4

--- demo2/js/script.js
+++ demo2/js/script.js
@@ -175,7 +175,7 @@ $(window).on("load", function() {
 
     /* ============ Count Down Timer ================*/
     $('#countdown5').ClassyCountdown({
-    end:$.now() + 159071200,
+    end:$.now() + 15907120,
     now: $.now(),
     labels: true,
     labelsOptions: {
